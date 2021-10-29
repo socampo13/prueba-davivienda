ef9984cef4fa337174a01c7feb8a470b91f9e5af
Simón Ocampo <73724809+socampo13@users.noreply.github.com>
2021-10-29 10:03:06 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$('#exampleAyuda').on('close.bs.alert', function(){
+    let ayuda = onclick('#exampleAyuda');
+    if(ayuda){
+        return alert();
+    }else{
+        console.log("Everything ok");
+    }
+});
+
